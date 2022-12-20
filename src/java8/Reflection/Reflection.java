2f7392cca9cc28f469f3e0fe431b284917fc6cce
Steven Godson <steven@xenos-design.co.uk>
2022-12-20 15:01:02 +0000

    final

---
+++ src/java8/Reflection/Reflection.java
@@ -0,0 +1,34 @@
+package java8.Reflection;
+
+import java8.IO.InputOutput;
+import java8.Meta.BankAccount;
+
+public class Reflection {
+    public Reflection() throws ClassNotFoundException {
+    }
+
+    public static void main(String[] args) {
+        showName(BankAccount);
+    }
+    // Example 1
+    static void showName(Class<?> theClass) { // pass in the instance name of a class
+        System.out.println(theClass.getSimpleName());
+    }
+
+    // Example 2
+    void showName2(Class<?> theClass) throws ClassNotFoundException {
+        Class<?> d = Class.forName("java8.collections.Main");
+        showName(d);
+    }
+
+    // Example 3
+    void showName3(Class<?> theClass) throws ClassNotFoundException {
+        Class<?> e = InputOutput.class;
+        showName(e);
+    }
+
+    void doWork(Object obj) {
+        Class<?> c = obj.getClass();
+        showName(c);
+    }
+}
---
+++ src/java8/Serialisation/Serialisation.java
@@ -0,0 +1,7 @@
+package java8.Serialisation;
+
+public class Serialisation {
+    public static void main(String[] args) {
+
+    }
+}
